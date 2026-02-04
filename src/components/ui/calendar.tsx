641854b71e093f5074310171861a7e60f364b5b5
AO PLUS <aoplusin@gmail.com>
2026-02-04 18:27:24 +0000

    Run npx --no-install next build ⚠ No build cache found. Please configure

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -54,8 +54,8 @@ function Calendar({
         ...classNames,
       }}
       components={{
-        IconLeft: ({ ...props }) => <ChevronLeft className="h-4 w-4" />,
-        IconRight: ({ ...props }) => <ChevronRight className="h-4 w-4" />,
+        IconPrevious: () => <ChevronLeft className="h-4 w-4" />,
+        IconNext: () => <ChevronRight className="h-4 w-4" />,
       }}
       {...props}
     />
